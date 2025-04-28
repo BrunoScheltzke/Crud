ce6cc7f0e0732e272fc0d2ef87f2d5c28460cd21
Bruno Scheltzke <brunofonten@gmail.com>
2025-04-28 07:42:32 -0300

    initial setup

---
+++ Crud/Crud/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Crud
+//
+//  Created by Bruno Scheltzke on 28/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Crud/Crud/CrudApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrudApp.swift
+//  Crud
+//
+//  Created by Bruno Scheltzke on 28/04/25.
+//
+
+import SwiftUI
+
+@main
+struct CrudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
